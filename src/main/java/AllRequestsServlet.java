c8e320035c9405024710739cd7ce6a5f0354640b
lia <nataly19999@gmail.com>
2021-12-21 12:43:40 +0300

    added PageGenerator and AllRequestsServlet

---
+++ src/main/java/AllRequestsServlet.java
@@ -0,0 +1,48 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AllRequestsServlet extends HttpServlet {
+    private String login = "";
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)// response - сюда пишем строку, которая потом превратится в страницу у клиента
+            throws ServletException, IOException {
+        Map<String, Object> pageVariables = createPageVariablesMap(request);
+        pageVariables.put("message", "");
+
+        response.getWriter().println(request.getParameter("key"));
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Map<String, Object> pageVariables = createPageVariablesMap(request);
+        String message = request.getParameter("message");
+        response.setContentType("text/html;charset=utf-8");
+
+        if (message == null || message.isEmpty()){
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        } else {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+        pageVariables.put("message", message == null ? "" : message);
+        response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
+    }
+
+    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request){
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("method", request.getMethod());
+        pageVariables.put("URL", request.getRequestURL());
+        pageVariables.put("pathInfo", request.getPathInfo());
+        pageVariables.put("sessionId", request.getSession());
+        pageVariables.put("parameters", request.getParameterMap().toString());
+
+        return pageVariables;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        System.out.println("server started");
+        server.join();
+    }
 }
